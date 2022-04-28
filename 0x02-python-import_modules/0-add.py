3b8814b2037d45944e1789f89a02c7d8900eb388
DaAsuncion <daniserafim_11@hotmail.com>
2022-04-28 11:34:46 -0700

    correct output 4

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __add_0__ == "__main__":
-    from add_0_ import add
+if __name__ == "__add_0__":
+    from add_0 import add
     a = 1
     b = 2
     print(f"{a} + {b} = {add(a, b)}")
