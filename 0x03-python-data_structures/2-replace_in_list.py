85207b990e5030147e30d8b1877a266bbe5a2fe2
DaAsuncion <daniserafim_11@hotmail.com>
2022-05-02 13:06:08 -0700

    checker error output again

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_element = 9
+
     if idx < 0 or idx > len(my_list) - 1:
-    return my_list
+        return my_list
     else:
-    return replace_in_list
+        return replace_in_list(my_list, idx, new_element)
