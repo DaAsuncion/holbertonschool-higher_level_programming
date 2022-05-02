1ec7e14ce8a9aa6d526c9855ec14a9338a410cdc
DaAsuncion <daniserafim_11@hotmail.com>
2022-05-02 11:17:55 -0700

    incorrect output 2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    my_list = [1, 2, 3, 4, 5];
-    return print_list_integer
+    my_list = [1, 2, 3, 4, 5]
+    print(f"print_list_integer")
+    print(*my_list, sep = "\n")
