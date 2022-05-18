08a7e4fc8f53fbdee0ed97bfb8199d557aecf067
DaAsuncion <daniserafim_11@hotmail.com>
2022-05-17 18:28:36 -0700

    new req error

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
 """Contains a class that defines a square.
-__size takes the size variable´s value passed as
-argument and checks if the size arg has a valid
-value"""
+Initialize each object by the
+__init__ method with a private instance variable called
+__size that takes the size variable's value passed as
+argument. Also checks if the size arg has a valid value.
+area method returns the area of the square.
+"""
 
 
 class Square:
-    """def a square"""
+    """Class that defines a square"""
     def __init__(self, size=0):
-        """init"""
-        self.__size = size
+        """initialize the __size attribute"""
+        self.size = size
 
-        @property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-        @size.setter
-        def size(self,value):
-            """set size to value"""
+    @size.setter
+    def size(self, value):
+        """set size attribute to value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
-
-                def area(self):
-                    """value of area"""
-                    return self.__size ** 2
+    def area(self):
+        """return the value of the square's area"""
+        return self.__size ** 2
