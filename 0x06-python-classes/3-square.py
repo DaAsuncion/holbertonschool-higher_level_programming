08699153f1f8a7bdbb0bf00d0d50a24970757b52
DaAsuncion <daniserafim_11@hotmail.com>
2022-05-17 18:06:57 -0700

    fail req.

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,7 +11,6 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
-    def area(self):
-        """returns square area"""
-        return self.__size * self.__size
+        def area(self):
+            """returns square area"""
+            return self.__size * self.__size
