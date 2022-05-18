831b71d2313f1c49fd4ae2f26bc7244dc9923a6c
DaAsuncion <daniserafim_11@hotmail.com>
2022-05-17 17:56:32 -0700

    requirment

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,14 +2,14 @@
 """Creating a new class called Square"""
 
 
-    class Square:
-        """class Square that defines a square by: (based on 1-square.py)"""
-        def __init__(self, size=0):
-            """Init square with size"""
-            self.__size = size
+class Square:
+    """class Square that defines a square by: (based on 1-square.py)"""
+    def __init__(self, size=0):
+        """Initialize Square with size attribute"""
+        self.__size = size
 
-            if type(size) is not int:
-                raise TypeError("size must be an integer")
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
 
-            if size < 0:
-                raise ValueError("size must be >= 0")
+        if size < 0:
+            raise ValueError("size must be >= 0")
