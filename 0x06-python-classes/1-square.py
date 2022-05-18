34ced0d76f157e759dde07d045115227299e109c
DaAsuncion <daniserafim_11@hotmail.com>
2022-05-17 17:38:22 -0700

    requirment

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,7 +2,7 @@
 """class Square that defines a square"""
 
 class Square:
-   """Write a class Square that defines a square by: (based on 0-square.py)"""
-   def __init__(self, size=0):
-       """init with size"""
-       self.__size = size
+    """Write a class Square that defines a square by: (based on 0-square.py)"""
+    def __init__(self, size=0):
+        """init with size"""
+        self.__size = size
