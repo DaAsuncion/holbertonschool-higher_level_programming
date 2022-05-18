2ad75a48e975d6ad4f5a9edd08ef1707685a5763
DaAsuncion <daniserafim_11@hotmail.com>
2022-05-17 18:08:52 -0700

    fail req.

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Creating a new class"""
+"""Creating a new class called Square"""
+
 
 class Square:
-    """Write a class Square that defines a square by: (based on 2-square.py)"""
-    def __init__(self,size=0):
-        """Init square with size"""
+    """class Square that defines a square by: (based on 1-square.py)"""
+    def __init__(self, size=0):
+        """Initialize Square with size attribute"""
         self.__size = size
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
+
         if size < 0:
             raise ValueError("size must be >= 0")
-        def area(self):
-            """returns square area"""
-            return self.__size * self.__size
+
+    def area(self):
+        """Method that returns the current square area"""
+        return self.__size * self.__size
