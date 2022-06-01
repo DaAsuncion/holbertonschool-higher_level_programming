fdbfe650bef80daf77dc8d7224975b3a33c301b6
DaAsuncion <daniserafim_11@hotmail.com>
2022-05-31 18:04:15 -0700

    requirment

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""write a function that reads a text file and prints it to stdout"""
+"""init"""
 
 def read_file(filename=""):
     """read a text file"""
     with open(filename, encoding="utf-8") as f:
         line = f.read()
-        print(line, end="")
-        f.close()
+    print(line, end="")
+    f.close()
